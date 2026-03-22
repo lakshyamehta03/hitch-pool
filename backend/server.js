58e077ba78784fa2d19e46c5fdfbdb1ff75c4e70
lakshyamehta03 <lakshyamehta03@gmail.com>
2026-03-22 23:53:41 +0530

    added vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 3000;
  * MIDDLEWARE
  * ============================================
  */
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
